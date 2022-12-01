b81e06807ffc88653f7ce10120174f4f2b2917f0
hutch28 <hutchinsjo@gcc.edu>
2022-12-01 11:24:45 -0500

    stub methods

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,28 @@
 public class Board {
+    private char[][] board;
+
+    public Board() {
+        // double for loop --- everything set ' '
+        //todo: grace
+    }
+
+    public void move(char player, int col) {
+        // throw exception if col oob
+        // throw exeptionif col is full
+        // update board
+        // todo:grace
+    }
+
+    public boolean hasWon(char plarer) {
+        // Todo: Corinne
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        // create a giant string with \n ... col numbers at top maybe?
+        // todo: grace
+        return null;
+    }
+
 }
---
+++ src/Game.java
@@ -0,0 +1,25 @@
+public class Game {
+    private Board b;
+    private char currPlayer;
+
+    // initially just 32 players ---
+    // but later change to take one param to indicate play against comp or human
+    public Game() {
+
+    }
+
+    public void gameLoop() {
+        //loop:
+        // print board
+        // talk to player, get next move
+        // if move is not an int or if move throws exception --- dont crash
+        // move
+        // check for has won
+        // change player
+    }
+
+    public int getCompMoveAI1() {
+        return 0;
+    }
+
+}
